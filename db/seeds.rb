c96e89f9baa209735bdc663320273371f64ebfb4
takumab <takumab@gmail.com>
2016-12-17 12:46:56 +0000

    Add price to products and some styling to photos page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(:name => "seed-generated-product", :description => "Also created this without using the HTML form")
+Product.create(:name => "Test 6", :description => "test 6",
+              :image_url => "beach_and_kite.jpg", :price => 11.99)
