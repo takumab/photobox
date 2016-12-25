136e801a8fc38b0bea9a7d13d22303f37f6f53fd
takumab <takumab@gmail.com>
2016-12-25 08:38:05 +0000

    Add some style and added orders controller and corresponding views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
   get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
 
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
